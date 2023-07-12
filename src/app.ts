6b7712edf9d31923d32d29439c14d2a782698433
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2023-07-12 20:56:18 +0600

    [DONE] eslint prettier husky lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express, { Application } from "express";
-import cors from "cors";
-const app: Application = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-export default app;
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+export default app
